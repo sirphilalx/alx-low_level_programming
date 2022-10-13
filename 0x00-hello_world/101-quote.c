d95c3808535cb821ce744a2c5b89b0611d686f73
sirphilalx <remotephil.upwork@gmail.com>
2022-10-13 16:06:23 -0700

    the last task that checks if we are able to make quotes using the C programming language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - Entry point
+ *main-program entry point.
  *
- * Return Always 1 (Success)
+ *Return: 1 on success.
  */
 int main(void)
 {
-	write(2," and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
