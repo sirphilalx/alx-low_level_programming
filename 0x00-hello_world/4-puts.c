486ecade047267ee54526d10978312007fe3ac4d
sirphilalx <remotephil.upwork@gmail.com>
2022-10-13 05:51:41 -0700

    this is not a script but a code in c programming language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[]="Programming is like building a multilingual puzzle";
+	puts (str);
+	return (0);
+}
