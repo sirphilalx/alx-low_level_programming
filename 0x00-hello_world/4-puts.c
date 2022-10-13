2d2ec89c04ed3544f10353acde043e93c157eacb
sirphilalx <remotephil.upwork@gmail.com>
2022-10-13 06:02:40 -0700

    I am committing too much of this file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return Always 0 (Success)
  */
-
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
