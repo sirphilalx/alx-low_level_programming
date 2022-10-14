984868ece0fbda6369bf839f818782d11f79227b
sirphilalx <remotephil.upwork@gmail.com>
2022-10-14 16:42:19 -0700

    I am always making corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *main-program entry point.
+ *Return:0-no error,non zero value if error.
+ **/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
+	if (n >  0)
 	{
 		printf("%d is positive\n", n);
-	}
+											}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);						}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
 	return (0);
 }
+
