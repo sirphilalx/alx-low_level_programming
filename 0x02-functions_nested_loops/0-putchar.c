85882282f1418ef42e4505ece01b05fe3df9c66a
sirphilalx <remotephil.upwork@gmail.com>
2022-10-18 06:07:42 -0700

    I wrote this one myself. A script that prints _putchar to the screen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main entry point where I create a putchar string
+ *
+ * Return Always 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);
+}
