bae6f5c37905144099048c938a5146256c4b2a29
sirphilalx <remotephil.upwork@gmail.com>
2022-10-19 04:19:12 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  *print_alphabet_x10 - prints the letters of the English alphabet 10
- *Description: Write a function that prints 10 times the alphabet, in lower
  */
 
 void print_alphabet_x10(void)
@@ -10,7 +9,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-	while (count++ < 9)
+	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
