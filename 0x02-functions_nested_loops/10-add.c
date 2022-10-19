430d949acd536fe68543164331830838b6e1dd48
sirphilalx <remotephil.upwork@gmail.com>
2022-10-19 13:59:27 -0700

    I did this one myself

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * add - the adder function name
+ * @a: the first number
+ * @b: the second number
+ * Return: always retuns 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
