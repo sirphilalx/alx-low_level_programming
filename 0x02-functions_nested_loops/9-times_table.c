08dd6c882f8697bc826b607e7e22376ab151ba2c
sirphilalx <remotephil.upwork@gmail.com>
2022-10-19 13:27:00 -0700

    9 task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,13 +16,6 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) * '0');
-
-			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
