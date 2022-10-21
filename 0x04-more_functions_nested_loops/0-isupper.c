7ebe0bba20ca9015f3388564325d9a4ab82e5a67
sirphilalx <remotephil.upwork@gmail.com>
2022-10-20 21:59:30 -0700

    first written code without looking anywhere. hope it works

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: the variable
+ * Return: returns 1 if uppercase otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	if (int c > 65)
+		return (1);
+	else
+		return (0);
+}
