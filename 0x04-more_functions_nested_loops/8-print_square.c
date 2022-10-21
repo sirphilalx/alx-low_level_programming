621b7b0cf8befda305956905bfe44773bfec9af5
sirphilalx <remotephil.upwork@gmail.com>
2022-10-21 06:56:27 -0700

    another tough one

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - the function that prints the square of a given number
+ * @size: the number that will be squared
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
