6f0e5f6b883aa63267f03ed044c0efb503010b71
sirphilalx <remotephil.upwork@gmail.com>
2022-10-20 22:37:40 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - the function
+ * @a: the first factor
+ * @b: the second factor
+ * Return: the multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
