94a689afe83141c51a41a1bb4a95a921f2fc4007
sirphilalx <remotephil.upwork@gmail.com>
2022-10-21 06:11:31 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,13 @@
 
 void print_line(int n)
 {
-	if (n == 0)
-		_putchar('\n');
-	else
-		_putchar('_' * n);
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
 	_putchar('\n');
 }
