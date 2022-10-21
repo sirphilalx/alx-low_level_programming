76e2f1448a4d21a3aca6dc911188e0d1a5e9f876
sirphilalx <remotephil.upwork@gmail.com>
2022-10-21 05:14:55 -0700

    I tried on this one

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main,h"
+
+/**
+ * print_most_numbers - this function prints most numbers
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a > 10)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+
+		a++;
+	}
+	_putchar('\n');
+}
