dadee724c225e7f20d6624325265834b39c16f9b
sirphilalx <remotephil.upwork@gmail.com>
2022-10-21 07:20:29 -0700

    the last mandatory task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry point
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i >= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		return (0);
+	}
+}
