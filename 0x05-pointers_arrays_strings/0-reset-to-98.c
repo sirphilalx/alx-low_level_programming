6da7a56207fc14b120a5d2d26ee841ad2f01751e
sirphilalx <remotephil.upwork@gmail.com>
2022-10-24 14:29:54 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset-to-98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - what we are resetting to
+ *
+ * @n: the variable whose pointer we are using
+ *
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
