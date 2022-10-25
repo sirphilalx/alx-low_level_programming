d64f6bed5a3b75e05f8370f3a7d47e081589f533
sirphilalx <remotephil.upwork@gmail.com>
2022-10-24 20:59:43 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - the function
+ * @a: the first int
+ * @n: the second n
+ * Return 0
+ */
+
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
