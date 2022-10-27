401ad32b47c89c927702b6d9a3f6bdd7150fbe5e
sirphilalx <remotephil.upwork@gmail.com>
2022-10-26 18:36:58 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @n: the pointer
+ * Return: char
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	while (n != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		n++;
+		}
+	}
+	return (n)
+}
