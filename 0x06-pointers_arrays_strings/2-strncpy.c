2c02649bd0d89bb94e3431c484499c8500d8e487
sirphilalx <remotephil.upwork@gmail.com>
2022-10-26 19:53:02 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.c"
+
+/**
+ * _strncpy - copies a string
+ * @dest: copies to
+ * @src: copies from
+ * @n: number of elements to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
