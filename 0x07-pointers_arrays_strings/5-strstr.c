6860c79be667296174bfef5985470e73f64a30e8
sirphilalx <remotephil.upwork@gmail.com>
2022-10-31 12:54:45 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main"
+
+/**
+ * *_strstr -  the functions
+ * @haystack: the pool
+ * @needle: what we are looking for
+ * Return: returns a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == haystack)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index + 1] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+			} while (hasytack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
