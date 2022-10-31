06ca879872b2d746f28de5d56ae592191ab87ad2
sirphilalx <remotephil.upwork@gmail.com>
2022-10-31 11:28:58 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - the function
+ * @s: the string
+ * @accept: the acceptance
+ * Return: returns th of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+}
