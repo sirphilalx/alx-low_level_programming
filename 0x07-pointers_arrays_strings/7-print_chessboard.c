4279ca0fcd9777401c01be1d4843d167c884f7d7
sirphilalx <remotephil.upwork@gmail.com>
2022-10-31 13:32:19 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessbaord - the function that prints an array of 2 dimension
+ * @a: the chessboard printed
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int index1;
+	int index2;
+
+	for (index1 = 0; a[index1][7]; index1++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+			_putchar(a[inex1][index2]);
+
+		_putchar('\n');
+	}
+}
