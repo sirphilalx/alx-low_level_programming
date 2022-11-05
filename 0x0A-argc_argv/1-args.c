e45beb41ff7721cd192561ff3bcb29107b8df62d
sirphilalx <remotephil.upwork@gmail.com>
2022-11-04 20:21:16 -0700

    task 1 prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry point
+ * @argc: the argument counter
+ * @argv: the argument mentioner
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
