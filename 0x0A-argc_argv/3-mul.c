ab9067d687a0dd3a45d52a890f54134036bc5a31
sirphilalx <remotephil.upwork@gmail.com>
2022-11-04 21:12:50 -0700

    returns mulitplication of 2 numbers otherwise prints an error message

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main entry point
+ * @argc: the counter
+ * @argv: the vector
+ * Return: returns 0 always
+ */
+
+int main(int argc, char argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argc[1]);
+	num2 = atoi(argc[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
