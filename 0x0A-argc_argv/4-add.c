214112c9b74a1f850edb0ae18fe9758093b71797
sirphilalx <remotephil.upwork@gmail.com>
2022-11-05 05:43:12 -0700

    function adds the arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main entry point
+ * @argc: the counter
+ * @argv: the vector
+ * Return: returns 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num, i, j, k;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+	}
+
+	for (k = 0; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d", sum);
+	return (0);
+}
