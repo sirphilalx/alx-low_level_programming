1160ec02c2d25a50555fc5872f9b8d190aee4cbb
sirphilalx <remotephil.upwork@gmail.com>
2022-11-05 05:53:56 -0700

    function adds the arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,15 +13,16 @@ int main(int argc, char *argv[])
 {
 	int result, num, i, j, k;
 
-	if (argc == 1)
-		printf("0\n");
 
 	for (i = 0; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
 		}
 	}
 
