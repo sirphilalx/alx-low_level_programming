3647fcd9a46b5f9d85fb16878251d544782c8103
sirphilalx <remotephil.upwork@gmail.com>
2022-11-04 20:28:40 -0700

    task 1 prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,6 +5,7 @@
  * main - the entry point
  * @argc: the argument counter
  * @argv: the argument mentioner
+ * Return: returns always 0
  */
 
 int main(int argc, char *argv[]__attribute__((unused)))
