c7004b73c46cce4dbfc242caabecb8b231f8e8ed
sirphilalx <remotephil.upwork@gmail.com>
2022-11-04 20:45:04 -0700

    prints out the arguments passed into it

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ * @argc: the argument counter
+ * @argv: the argument mentioner
+ * Return: returns always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i])
+	}
+	printf("\n");
+
+	return (0);
