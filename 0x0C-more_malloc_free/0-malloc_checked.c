fa8a86d2697be8375ec1d6fd97c457a02921e108
Philemon Okpokpa <113598609+sirphilalx@users.noreply.github.com>
2022-11-12 05:20:16 +0100

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* malloc_checked - uses malloc to allocate space in the heap memory
-* @b: the integer to be checked
-* Return: void
-*/
-
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
@@ -13,9 +14,7 @@ void *malloc_checked(unsigned int b)
 	ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
 		exit(98);
-	}
 
 	return (ptr);
 }
