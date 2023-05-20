96b3ae65eb3472d48dcce82831b16c69a6a751dd
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-05-20 01:35:01 +0100

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	int i;
+	char hex[] = "123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex[i]);
+	}
+	putchar('\n');
+	return (0);
+}
