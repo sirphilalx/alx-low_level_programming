fd112f1bf0aaaaea212881eb3044a4076e3e3558
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-05-20 01:07:41 +0100

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
