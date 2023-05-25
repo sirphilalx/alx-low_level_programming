c207d6ba3e0ebbca6fe8b1cdcb46f6d6e0bed683
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-05-26 00:07:51 +0100

    print numbers 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - the function that prints numbers 1 through 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
