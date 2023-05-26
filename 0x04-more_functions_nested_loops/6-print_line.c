18d4acea4fcb0eded188316518fb30f46d5160c9
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-05-26 23:56:00 +0100

    hot fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,9 @@
  */
 void print_line(int n)
 {
+	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
-
-		for (i = 0; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
-	}
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
 }
