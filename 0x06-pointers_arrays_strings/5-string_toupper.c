e12ba39f2c7d2be50db658be9dea70bc103548f6
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-02 20:49:43 +0100

    converts lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - the function that gets called
+ * @str: the string that gets converted
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
