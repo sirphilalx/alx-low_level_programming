c309de1e78ca4b7793880a218e50eaa89ce8404d
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-03 02:21:56 +0100

    rot-13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - the encoder function
+ * @str: the string that gets encoded
+ */
+char *rot13(char *str)
+{
+	int i;
+	int j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data1[j])
+			{
+				str[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
