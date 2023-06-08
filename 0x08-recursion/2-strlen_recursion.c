b6a530e747ba60942f97549d5f9d2c3869de2b2b
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-08 22:56:47 +0100

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - the function name
+ * @s: the string that gets played on
+ */
+int _strlen_recursion(char *s)
+{
+	int k;
+
+	k = 0;
+	if (*s)
+	{
+		k++;
+		k += _strlen_recursion(s + 1);
+	}
+	return (k);
+}
