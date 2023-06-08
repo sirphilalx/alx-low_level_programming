a74ba9efaae7b821dac8263e03c2223c185d3d84
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-08 23:34:21 +0100

    power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - the function that gets called
+ * @x: the operant
+ * @y: the operand
+ * Return: returns -1 if y is lower than 0 otherwise returns x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	else
+		return (x * _pow_recursion(x, y - 1));
+	return (x);
+}
