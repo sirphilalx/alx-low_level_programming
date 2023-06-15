868c7c6b3d36473d0946cc267e5594510b44f5ce
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-15 03:43:09 +0100

    multiplier

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
 	int result = 1;
 
 	if (argc <= 1)
@@ -17,14 +18,10 @@ int main(int argc, char *argv[])
 		printf("%s\n", "Error");
 		return (1);
 	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	result = i * j;
 
-	if
-	{
-		for (i = 1; i < argc; i++)
-		{
-			result *= atoi(argv[i]);
-		}
-		printf("%d\n", result);
-		return (0);
-	}
+	printf("%d\n", result);
+	return (0);
 }
