0e1a075f74ead039ae9a8d9ba50c42e9f9f4c552
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-19 15:31:32 +0100

    file path

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - the entry point of the program
+ * Return: returns 0 on success
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return 0;
+	return (0);
 }
