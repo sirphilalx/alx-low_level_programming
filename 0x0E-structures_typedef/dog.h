c5dee78d79f77ca714530650673ecd24b79cb7ea
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-20 01:17:59 +0100

    the dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * struct dog - the dog struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: the dog struct will be used to create several
+ * instances of dogs
+ *
+ */
+ struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
