72450da453117d8db885c868d0681836afc24369
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-06-23 11:49:43 +0100

    creating a struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - the function name
+ * @d: the struct name
+ * @name: this would be the name of the dog
+ * @age: a float of a number that represents the age of the dog
+ * @owner: this would be the name of the owner of the sun
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
+	:
+}
