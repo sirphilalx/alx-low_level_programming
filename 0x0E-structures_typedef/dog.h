6e4eaa4255b89530cbde89b613b8f2ccb16609e4
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-01 17:18:25 +0100

    dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
+ * dog_t - Typedef for the struct dog
  */
 typedef struct dog dog_t;
 
@@ -27,5 +27,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char* _strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif /* DOG_H */
