fb7751c0a442765273d6c83d7b418ade3b0c328f
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-04 15:22:53 +0100

    hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,8 +18,7 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		printf("(nil)");
 
-
-	printf("Name: %s\n", d->name);
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
