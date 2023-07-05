99ee1096987614710f96e233c5e6cecaa5fd040a
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-05 07:45:08 +0100

    lists number of elements in the linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
