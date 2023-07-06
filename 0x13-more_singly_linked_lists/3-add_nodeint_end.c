cd43f018e6253e6984c5a5d1a1b5e64c3818e1d1
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-06 11:55:48 +0100

    T3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *last = NULL;
+	listint_t *last = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -23,15 +23,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->next = NULL;
 
 	if (*head == NULL)
-		*head = new;
-
-	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		*head = new;
+		return (new);
 	}
 
-	return (last);
+	while (last->next)
+		last = last->next;
+
+	last->next = new;
+
+	return (new);
 }
