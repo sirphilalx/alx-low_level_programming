1f5b82f21372a4575cf0d860716823d9ce27e485
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-09 23:49:30 +0100

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
  * get_endianness - the function
- *
- * Returns: 0 if big endian, 1 if little endian
+ * union: Create a union to store the value
+ *	of 1 in both big endian and little endian.
+ * Return: 0 if big endian, 1 if little endian
  */
 
-int get_endianness(void) {
-  int endianness;
-  union {
-    unsigned int i;
-    char c[4];
-  } u;
-  u.i = 1;
-  endianness = u.c[0] == 1;
-  return endianness;
+int get_endianness(void)
+{
+	int endianness;
+
+	union
+	{
+		unsigned int i;
+		char c[4];
+	}
+	u;
+	u.i = 1;
+	endianness = u.c[0] == 1;
+	return (endianness);
 }
