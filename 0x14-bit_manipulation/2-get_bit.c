d351680ef190c2b5a23e9a1484cd1250896aca94
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-09 22:24:39 +0100

    gets bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,10 +9,12 @@
  */
 
 
-int get_bit(unsigned long int n, unsigned int index) {
-  if (index >= 32) {
-    return -1;
-  }
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= 32)
+	{
+		return (-1);
+	}
 
-  return (n >> index) & 1;
+	return ((n >> index) & 1);
 }
