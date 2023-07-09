8202b7655ce533e39e86f50f9a45d92046a78404
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-09 22:22:22 +0100

    gets bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - the function
+ * @n: first parameter
+ * @index: second index
+ *
+ * Return: returns the bit at index
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index) {
+  if (index >= 32) {
+    return -1;
+  }
+
+  return (n >> index) & 1;
+}
