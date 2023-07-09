c9f15e32cd37681761f5e94ded876f2923ba0209
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-09 21:08:21 +0100

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - function
+ * @b: the binary
+ * 
+ * Return: return the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		value = value << 1;
+		value |= (b[i] - '0');
+		i++;
+	}
+
+	return (value);
+}
