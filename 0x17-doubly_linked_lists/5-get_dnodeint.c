8d12d749c7b8e39c3cb5fee480cacf283bd4846a
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-08-19 00:23:01 +0100

    get node integer

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,19 +1,25 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - sums all of the data of a dlistint_t linked list
- * @head: pointer to the beginning of the linked list
+ * get_dnodeint_at_index - finds a specific node of a linked list
+ * @head: pointer to the beginning of the list
+ * @index: index of the node to retrieve
  *
- * Return: sum of all data, or 0 if the list is empty
+ * Return: pointer to the indexed node, or NULL on failure
  */
-int sum_dlistint(dlistint_t *head)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int sum = 0;
+	unsigned int i;
 
-	while (head != NULL)
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	for (i = 0; i < index; i++)
 	{
-		sum += head->n;
+		if (head->next == NULL)
+			return (NULL);
 		head = head->next;
 	}
-	return (sum);
+	return (head);
 }
